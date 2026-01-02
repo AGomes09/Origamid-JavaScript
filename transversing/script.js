c35a26da33291773a85a66a1f9599a2cdd8fd839
Adriano <adriano.almeida09@outlook.com>
2026-01-02 16:55:01 -0300

    Primeira parte sobre transversing e manipulação

---
+++ transversing/script.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector("h1");
+const animaisLista = document.querySelector(".animais-descricao");
+// h1.outerHTML = "<p>Novo Título</p>";
+
+// console.log(animaisLista.innerHTML);
+
+const lista = document.querySelector(".animais-lista");
+
+console.log(lista.children[--lista.children.length]);
+console.log(lista.querySelector("li:last-child"));
